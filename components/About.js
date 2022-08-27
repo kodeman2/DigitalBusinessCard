8db07cae2327532b5f0d5e64c1ba1a38a7752873
Olagunju Oluwakolade <97587019+kodeman2@users.noreply.github.com>
2022-08-27 21:37:43 +0100

    updated typo

--- components/About.js
+++ components/About.js
@@ -4,7 +4,7 @@ export default function About(){
     return (
         <div className = "About">
         <h3 className="About-title">About</h3>
-        <p className="About-info">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        <p className="About-info">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and i'am always looking for new things to learn.</p>
         </div>
     )
 }
